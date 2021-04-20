02eeac3d338285fcf1e35124bc2a94644c12bd9a
Constantin Dimitrenco <constantin.dimitrenco@gmail.com>
2021-04-20 19:21:22 +0000

    asd

--- hello_constantin/Program.cs
+++ hello_constantin/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello from Constantin!");
-        }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, world!");
+        Console.ReadLine();
+    }
     }
 }
